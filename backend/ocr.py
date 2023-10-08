e52784bb3124f44b015c32d3831218c93c9b3014
Bobliuuu <mmmzzz66g@gmail.com>
2023-10-08 07:34:46 -0400

    Edit files

--- backend/ocr.py
+++ backend/ocr.py
@@ -1,22 +1,25 @@
 import easyocr
 import openai
 
-openai.api_key = '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'
+openai.api_key = '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'
 
 reader = easyocr.Reader(['en']) 
-result = reader.readtext('linear algebra.png', detail = 0)
+result = reader.readtext('photos/linear algebra.png', detail = 0)
 text = " ".join(result)
 
 prompt = "Given this text, return whether it is related or not to linear algebra. Only return yes or no. Do not return anything else other than yes or no."
 
-response = openai.Completion.create(engine='',
-                                    prompt=prompt,
-                                    max_tokens=1024,
-                                    temperature=0,
-                                    top_p=1,
-                                    frequency_penalty=0.0,
-                                    presence_penalty=0.0,
-                                    stop=['#'])['choices'][0]['text']
+response = openai.ChatCompletion.create(
+    model='gpt-3.5-turbo',
+    messages = [
+        {'role': 'user', 'content': prompt}
+    ],
+    max_tokens=1024,
+    temperature=0,
+    top_p=1,
+    frequency_penalty=0.0,
+    presence_penalty=0.0,
+    stop=['#'])['choices'][0]["message"]
 
 print(response)
 
--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -16,6 +16,38 @@ chrome.tabs.query({ active: true, lastFocusedWindow: true }, function(tabs) {
   });
 });
 
+const apiKey = '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';
+
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs){
-    chrome.tabs.remove(tabs[0].id);
+    let tabList = document.getElementById("tabList");
+    tabs.forEach(function(tab) {
+        var activeTabId = tab.id;
+        let executeScriptPromise = new Promise((resolve, reject) => {
+            chrome.scripting.executeScript({
+                target: { tabId: activeTabId },
+                injectImmediately: true,
+                func: DOMtoString,
+            }, (results) => {
+                if (chrome.runtime.lastError) {
+                    reject(new Error(chrome.runtime.lastError));
+                } else {
+                    resolve(results);
+                }
+            });
+        });
+
+        executeScriptPromises.push(executeScriptPromise);
+
+        Promise.all(executeScriptPromises)
+        .then((resultsArray) => {
+            // Append each result to tabList
+            resultsArray.forEach((results) => {
+                tabList.appendChild(document.createTextNode(results));
+            });
+        })
+        .catch((error) => {
+            tabList.appendChild(document.createTextNode(error.message));
+        });
+    })
+    //chrome.tabs.remove(tabs[0].id);
 });
