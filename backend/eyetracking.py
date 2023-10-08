128db3b010ce5ea5a9f7618c77ab6220446d82bb
Bobliuuu <mmmzzz66g@gmail.com>
2023-10-07 21:29:29 -0400

    Added stuff

--- backend/eyetracking.py
+++ backend/eyetracking.py
@@ -10,6 +10,10 @@ while True:
     result = DeepFace.analyze(frame,actions = ['emotion'])
     emotions = result['emotion']
     print(emotions)
+    _, img = cap.read()
+    #result = DeepFace.analyze(frame,actions = ['emotion'])
+    #emotions = result['emotion']
+    #print(emotions)
     # Lie detection below
     """
     lie = emotions['angry'] + emotions['disgust'] + emotions['fear'] + emotions['sad']
@@ -18,8 +22,21 @@ while True:
         text = "Truth"
     else:
         text = "Stop lying to me you asshole"
-    cv2.putText(frame, text, (50,50), font, 3, (255,0,0), 2, cv2.LINE_4)
+    cv2.putText(img, text, (50,50), font, 3, (255,0,0), 2, cv2.LINE_4)
     """
+    faces = faceCascade.detectMultiScale(gray, 1.1, 4)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    for(x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        face = frame[y: y+h, x: x+w]
+        eyes = eyeCascade.detectMultiScale(gray)
+        eyes_open = 0
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(img, (ex, ey), (ex + ew, ey + eh), (255, 0, 0), 2)
+            eyes_open += 1
+    # If eyes_open < 2, sleeping (pay attention)
+    # If faces = None, not looking (go look)
+    # Else, get emotion and check
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
--- backend/ocr.py
+++ backend/ocr.py
@@ -1,4 +1,23 @@
 import easyocr
+import openai
+
+openai.api_key = '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'
+
 reader = easyocr.Reader(['en']) 
 result = reader.readtext('linear algebra.png', detail = 0)
-print(" ".join(result))
+text = " ".join(result)
+
+prompt = "Given this text, return whether it is related or not to linear algebra. Only return yes or no. Do not return anything else other than yes or no."
+
+response = openai.Completion.create(engine='',
+                                    prompt=prompt,
+                                    max_tokens=1024,
+                                    temperature=0,
+                                    top_p=1,
+                                    frequency_penalty=0.0,
+                                    presence_penalty=0.0,
+                                    stop=['#'])['choices'][0]['text']
+
+print(response)
+
+
